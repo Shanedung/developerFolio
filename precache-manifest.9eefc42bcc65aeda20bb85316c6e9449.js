f894434ee1f28b9f18d1e727f0b4fc61498d85cf
Dung Nguyen <dungnguyen@Dungs-MacBook-Pro.local>
2021-03-24 02:30:53 +0700

    Updates

---
+++ precache-manifest.9eefc42bcc65aeda20bb85316c6e9449.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6eb7444fc508d4193e0199d6d593540",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "3a9430a77c3683510538",
+    "url": "/developerFolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "7aacc93bb13b5bcc1cc1",
+    "url": "/developerFolio/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "ac40480c28e53d4c741c",
+    "url": "/developerFolio/static/css/main.42d6baf3.chunk.css"
+  },
+  {
+    "revision": "e2545affc43d8a0347a1",
+    "url": "/developerFolio/static/js/2.062dc7ac.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/developerFolio/static/js/2.062dc7ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a9430a77c3683510538",
+    "url": "/developerFolio/static/js/3.13553e10.chunk.js"
+  },
+  {
+    "revision": "7aacc93bb13b5bcc1cc1",
+    "url": "/developerFolio/static/js/4.2eade62e.chunk.js"
+  },
+  {
+    "revision": "ac40480c28e53d4c741c",
+    "url": "/developerFolio/static/js/main.6d4ad179.chunk.js"
+  },
+  {
+    "revision": "27e41f37112c8dcf11ca",
+    "url": "/developerFolio/static/js/runtime-main.db8f9aef.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "284f16aacb480141149392f4ddfdb861",
+    "url": "/developerFolio/static/media/Coats_logo.284f16aa.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e638b6ca0c574552630392d369fb21e8",
+    "url": "/developerFolio/static/media/bachkhoa.e638b6ca.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/developerFolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/developerFolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/developerFolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/developerFolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/developerFolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fe3355d62032bf53790bb0c74d9a7110.js"
+  "/developerFolio/precache-manifest.9eefc42bcc65aeda20bb85316c6e9449.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/developerFolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
